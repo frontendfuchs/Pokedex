c75cb3ac01bbecefa22ab5c9f0aba1f1c29ac851
frontendfuchs <adriana.letitia@yahoo.com>
2026-06-14 23:28:40 +0200

    feat: add getData function for pokemon list

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+async function getData() {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=40&offset=0');
+    const responseFromJson = await response.json();
 
+    console.log(responseFromJson.results);
+}
+
+getData();
