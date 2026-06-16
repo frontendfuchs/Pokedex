36f9a071334aaf3686be2e7e6e81b3158893b47b
frontendfuchs <adriana.letitia@yahoo.com>
2026-06-16 23:05:24 +0200

    feat(modal): create modal component for pokemon details and style

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let allPokemon = [];
+let currentPokemonIndex = 0;
 
 async function getData() {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=40&offset=0');
@@ -12,14 +14,17 @@ async function getData() {
 async function renderPokemonCards(pokemonList) {
     const containerRef = document.getElementById("pokemon-cards-container");
     containerRef.innerHTML = "";
+    allPokemon = [];
 
     for (let index = 0; index < pokemonList.length; index++) {
         const pokemonResponse = await fetch(pokemonList[index].url);
         const pokemonData = await pokemonResponse.json();
+
+         allPokemon.push(pokemonData);
         
         const pokemonTypes = getPokemonTypes(pokemonData);
         containerRef.innerHTML += getPokemonCardTemplate(pokemonData,index,pokemonTypes[0]);
-        const pokemonType = renderPokemonTypes(pokemonTypes,index);
+        renderPokemonTypes(pokemonTypes, index);
     }
 }
 
@@ -42,4 +47,53 @@ function renderPokemonTypes(pokemonTypes,index){
     }
 }
 
+function openPokemonDialog(index) {
+    currentPokemonIndex = index;
+
+    const dialogRef = document.getElementById("dialog");
+    dialogRef.innerHTML = getPokemonDialogTemplate(allPokemon[currentPokemonIndex]);
+    dialogRef.showModal();
+}
+
+function closePokemonDialog() {
+    document.getElementById("dialog").close();
+}
+
+function getDialogTypesTemplate(types) {
+    let html = "";
+
+    for (let i = 0; i < types.length; i++) {
+        html += `
+            <div class="pokemon-type-${types[i].type.name}">
+                <p>${types[i].type.name}</p>
+            </div>
+        `;
+    }
+    return html;
+}
+
+function showPreviousPokemon() {
+    if (currentPokemonIndex === 0) {
+        currentPokemonIndex = allPokemon.length - 1;
+    } else {
+        currentPokemonIndex--;
+    }
+
+    const dialogRef = document.getElementById("dialog");
+    dialogRef.innerHTML = getPokemonDialogTemplate(allPokemon[currentPokemonIndex]);
+}
+
+function showNextPokemon() {
+    if (currentPokemonIndex === allPokemon.length - 1) {
+        currentPokemonIndex = 0;
+    } else {
+        currentPokemonIndex++;
+    }
+
+    const dialogRef = document.getElementById("dialog");
+    dialogRef.innerHTML = getPokemonDialogTemplate(allPokemon[currentPokemonIndex]);
+}
+
+
 getData();
+
--- scripts/template.js
+++ scripts/template.js
@@ -1,7 +1,7 @@
 
 function getPokemonCardTemplate(pokemonData,index,pokemonType) {
     return `
-        <div class="pokemon-card-${pokemonType}">
+        <div class="pokemon-card-${pokemonType}" onclick="openPokemonDialog(${index})">
         <div class="pokemon-card">
             <h2>${pokemonData.name}</h2>
             <p>#${pokemonData.id}</p>
@@ -22,4 +22,28 @@ function getPokemonTypesTemplate(pokemonType) {
     <p>${pokemonType}</p>
     </div>
     `; 
+}
+
+function getPokemonDialogTemplate(pokemonData) {
+    return `
+        <div class="pokemon-dialog ${pokemonData.types[0].type.name}">
+            <button class="close-btn" onclick="closePokemonDialog()">X</button>
+            <button class="arrow-btn left" onclick="showPreviousPokemon()">&#10094;</button>
+            <button class="arrow-btn right" onclick="showNextPokemon()">&#10095;</button>
+            <div class="pokemon-dialog-header">
+                <h2>${pokemonData.name}</h2>
+                <p>#${pokemonData.id}</p>
+            </div>
+            <div class="pokemon-dialog-image">
+                <img src="${pokemonData.sprites.other['dream_world'].front_default}" alt="${pokemonData.name}">
+            </div>
+            <div class="pokemon-dialog-types">
+                ${getDialogTypesTemplate(pokemonData.types)}
+            </div>
+            <div class="pokemon-dialog-infos">
+                <p>Height: ${pokemonData.height}</p>
+                <p>Weight: ${pokemonData.weight}</p>
+            </div>
+        </div>
+    `;
 }
