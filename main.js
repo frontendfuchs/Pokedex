1ef36ec52229e19945dba5f2617b7585f1acb9d6
frontendfuchs <adriana.letitia@yahoo.com>
2026-06-19 11:16:33 +0200

    refactor(css): replace fixed sizes with clamp() and responsive for mobile

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
-let allPokemon = [];
-let currentPokemonIndex = 0;
-let offset = 0;
-const limit = 20;
-let filteredPokemon = [];
+let allPokemon = [];  // every pokemon is fetched gets pushed into allPokemon,contains the full pokemon data objects that we later need for cards, dialog, stats and abilities
 
+let currentPokemonIndex = 0;  //stores which pokemon is currently shown inside the dialog
+let offset = 0; // offset and limit are used for pagination, so loadMorePokemon()
+const limit = 20;
+let filteredPokemon = []; // filteredPokemon is a second array for search results
+
+
+// the first api endpoint only gives a short list with name + url
+// we pass responseFromJson.results to renderPokemonCards()
+// and inside that function we fetch every single pokemon again by its own url
 async function getData() {
   const response = await fetch(
     `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`,
   );
   const responseFromJson = await response.json();
 
-  console.log(responseFromJson.results);
-
-  // renderPokemonCards(responseFromJson.results);
   await renderPokemonCards(responseFromJson.results);
+
+  filteredPokemon = allPokemon;
 }
 
+
+// this function gets the pokemon list from the first api call
+// then we loop through that list and fetch the full details for each single pokemon
+// after fetching, we push every full pokemon object into allPokemon
+// we use currentIndex for ids and for opening the correct dialog later
 async function renderPokemonCards(pokemonList) {
   const containerRef = document.getElementById("pokemon-cards-container");
 
@@ -37,11 +46,15 @@ async function renderPokemonCards(pokemonList) {
   }
 }
 
+// this function loads the next pokemons
+// offset gets increased by limit, so the next api request skips the already loaded pokemon
 function loadMorePokemon() {
     offset += limit;
     getData();
 }
 
+// this function extracts the type names from one pokemon object
+// pokemonData is one single pokemon object with many properties
 function getPokemonTypes(pokemonData) {
   let pokemonTypes = [];
 
@@ -51,6 +64,9 @@ function getPokemonTypes(pokemonData) {
   return pokemonTypes;
 }
 
+// this function renders the visible type inside one pokemon card
+// index is the card index that we use to find the correct container
+// we clear the type container first so it does not duplicate content on re-render
 function renderPokemonTypes(pokemonTypes, index) {
   const pokemonTypesRef = document.getElementById(`pokemon-types-${index}`);
   pokemonTypesRef.innerHTML = "";
@@ -60,6 +76,11 @@ function renderPokemonTypes(pokemonTypes, index) {
   }
 }
 
+// this function opens the dialog for the clicked pokemon card
+// index is the index from allPokemon, we store that index globally in currentPokemonIndex
+// important for the next and previous buttons in the dialog
+// we need to know which pokemon is currently active
+// after that we render the full dialog for exactly this pokemon
 function openPokemonDialog(index) {
   currentPokemonIndex = index;
 
@@ -70,9 +91,14 @@ function openPokemonDialog(index) {
   dialogRef.showModal();
 }
 
+// this function closes the open dialog
+// we access the dialog directly by id and call the close() method
 function closePokemonDialog() {
   document.getElementById("dialog").close();
 }
+
+// this function builds the html string for the pokemon types inside the dialog
+// html starts as an empty string and gets filled step by step in the loop
 
 function getDialogTypesTemplate(types) {
   let html = "";
@@ -87,6 +113,9 @@ function getDialogTypesTemplate(types) {
   return html;
 }
 
+// this function switches the dialog to the previous pokemon
+// if we are currently at the first pokemon, we jump to the last one
+// after changing currentPokemonIndex we re-render the dialog content
 function showPreviousPokemon() {
   if (currentPokemonIndex === 0) {
     currentPokemonIndex = allPokemon.length - 1;
@@ -100,6 +129,10 @@ function showPreviousPokemon() {
   );
 }
 
+// this function switches the dialog to the next pokemon
+// if we are at the last pokemon, we jump back to the first one
+// this only works with pokemon that are already inside allPokemon
+// not with pokemon that are not loaded yet from the api
 function showNextPokemon() {
   if (currentPokemonIndex === allPokemon.length - 1) {
     currentPokemonIndex = 0;
@@ -113,6 +146,9 @@ function showNextPokemon() {
   );
 }
 
+// this function creates the stats tab html for one pokemon
+// stats is the stats array from the api
+// each entry contains a stat object and the base_stat value
 function getStatsTemplate(stats) {
   let html = '<div class="stats-content">';
 
@@ -129,6 +165,8 @@ function getStatsTemplate(stats) {
   return html;
 }
 
+// this function creates the abilities tab html for one pokemon
+// abilities is also the  api array and each ability name is nested inside .ability.name
 function getAbilitiesTemplate(abilities) {
   let html = '<div class="abilities-content">';
 
@@ -144,6 +182,11 @@ function getAbilitiesTemplate(abilities) {
   return html;
 }
 
+// this function controls the tab switching inside the dialog
+// tabId is the id of the tab content that should become visible
+// clickedButton is the exact button that was clicked
+// first we remove "active" from all tab contents and all buttons
+// after that we activate only the selected content and the clicked button
 function openTab(tabId, clickedButton) {
   const allTabContents = document.querySelectorAll(".tab-content");
   const allTabButtons = document.querySelectorAll(".tab-btn");
@@ -160,6 +203,8 @@ function openTab(tabId, clickedButton) {
   clickedButton.classList.add("active");
 }
 
+
+/* DIALOG EVENT */
 const dialog = document.getElementById("dialog");
 
 dialog.addEventListener("click", (event) => {
@@ -168,6 +213,12 @@ dialog.addEventListener("click", (event) => {
   }
 });
 
+
+// this function checks the search input while the user is typing
+// trim() removes spaces at the beginning and end so empty spaces are not counted as real input
+// search only becomes allowed from 3 characters on, before that the search button stays disabled
+// if the input is completely empty again, we reset filteredPokemon back to allPokemon
+// and render the full list again
 function searchInput() {
   let value = document.getElementById("pokemon-search").value.trim();
 
@@ -188,6 +239,11 @@ function searchInput() {
   }
 }
 
+// this function is the actual search after clicking the search button
+// we convert the input to lower case so the search becomes case-insensitive
+// .filter() goes through the full allPokemon array and checks every loaded pokemon
+// the function returns only the pokemon whose names include the typed search text
+// the result gets stored in filteredPokemon and then renderFilteredPokemon() shows only that result list
 function searchPokemon() {
   let value = document.getElementById("pokemon-search").value.toLowerCase().trim();
 
@@ -205,6 +261,12 @@ function searchPokemon() {
   renderFilteredPokemon();
 }
 
+// this function renders only the pokemon that are currently inside filteredPokemon
+// first we clear the whole card container so the old cards disappear
+// then we loop through filteredPokemon and build the cards again
+//index here is only the index inside filteredPokemon
+// but for opening the correct dialog we need the index from allPokemon
+// that is why we use allPokemon.indexOf(pokemonData)
 function renderFilteredPokemon() {
   let container = document.getElementById("pokemon-cards-container");
   container.innerHTML = "";
@@ -224,6 +286,4 @@ function renderFilteredPokemon() {
   }
 }
 
-getData();
-filteredPokemon = allPokemon;
-renderFilteredPokemon();
+
